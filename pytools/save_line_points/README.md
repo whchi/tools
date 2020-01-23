4dc94d56ad3c9cdb93011631204e5a232c8266c2
DylanChi <dylanchi@cw.com.tw>
2020-01-23 22:27:01 +0800

    little tool

---
+++ pytools/save_line_points/README.md
@@ -0,0 +1,23 @@
+### 前言
+因為某次意外，抽到了 10000 點 line points，拿到的時候居然是 10x1000 個 uri...\
+所以寫了這個程式處理它
+### demo
+[![sample](https://img.youtube.com/vi/CYc8CBqm5Lw/0.jpg)](https://www.youtube.com/watch?v=CYc8CBqm5Lw&feature=youtu.be)
+### 作法
+1. 一份滿滿的兌換 uri xlsx，組成如下
+
+||
+|:--|
+|https://points.line.me/pointcode?pincode=XXXXX|
+|https://points.line.me/pointcode?pincode=XXXXX|
+|https://points.line.me/pointcode?pincode=XXXXX|
+|https://points.line.me/pointcode?pincode=XXXXX|
+2. 使用 pandas 讀取它，擷取 pointcode 字串作為每次 loop 的 input
+3. 使用 selenium 模擬操作行為
+### 用法
+0. **擁有一份得獎清單**
+1. cfg.txt 裡面設定 line 帳密以及要不要看到瀏覽器
+2. 建立 toskip.txt
+3. `pip install -r requiremennts.txt`
+4. `python main.py`
+5. wait until finish
---
+++ pytools/save_line_points/main.py
@@ -0,0 +1,89 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DRIVER_PATH = BASE_DIR + '/chromedriver'
+CHROME_OPTIONS = Options()
+settings = {}
+with open(f'{BASE_DIR}/cfg.txt', 'r') as f:
+    for line in f.readlines():
+        line = line.strip('\n').split('=')
+        settings[line[0]] = line[1]
+with open(f'{BASE_DIR}/toskip.txt', 'r') as f:
+    to_skip = list(filter(None, f.readline().strip('\n').split(',')))
+CHROME_OPTIONS.headless = False if settings['SEE_BROWSER'] == 'y' else True
+
+
+def gogogo(account, passwd):
+    df = pd.read_excel(f'{BASE_DIR}/pincodes.xlsx', header=None)
+    df.columns = ['uri']
+
+    uri_prefix = 'https://points.line.me/pointcode?pincode='
+    point_codes = df.uri.apply(lambda s: s[len(uri_prefix):]).tolist()
+    not_login_uri_prefix = 'https://access.line.me/dialog/oauth/weblogin'
+    browser = webdriver.Chrome(executable_path=DRIVER_PATH,
+                               options=CHROME_OPTIONS)
+
+    browser.get(uri_prefix)
+    new_skip = to_skip
+    try:
+        for code in point_codes:
+            if code in to_skip:
+                continue
+            print(code)
+            page_src = browser.page_source
+            if '此點數代碼已兌換完畢。' in page_src:
+                new_skip.append(code)
+                time.sleep(3)
+            elif '錯誤' in page_src or '30分鐘' in page_src:
+                time.sleep(3)
+                raise Exception('input rate limit')
+
+            browser.implicitly_wait(1)
+
+            pin_code = browser.find_element_by_id('pincode')
+            send_btn = browser.find_element_by_css_selector(
+                'form div.MdBtn01>button[type="submit"]')
+            pin_code.clear()
+            pin_code.send_keys(code)
+            send_btn.send_keys(Keys.ENTER)
+            if not_login_uri_prefix in browser.current_url:
+                # login
+                browser.find_element_by_id('id').send_keys(account)
+                browser.find_element_by_id('passwd').send_keys(passwd)
+                time.sleep(0.1)
+                browser.find_element_by_css_selector(
+                    'input[type="submit"]').send_keys(Keys.ENTER)
+                time.sleep(0.3)
+
+                if '此點數代碼已兌換完畢。' in page_src:
+                    new_skip.append(code)
+                    time.sleep(3)
+                elif '錯誤' in page_src or '30分鐘' in page_src:
+                    time.sleep(3)
+                    raise Exception('input rate limit')
+
+            new_skip.append(code)
+            browser.find_element_by_css_selector(
+                '#mainView div.MdBtn01>a').send_keys(Keys.ENTER)
+
+    except WebDriverException as we:
+        raise we
+    except Exception as e:
+        raise e
+    finally:
+        with open(f'{BASE_DIR}/toskip.txt', 'w') as f:
+            f.write(','.join(new_skip))
+
+        browser.close()
+
+
+if __name__ == '__main__':
+    gogogo(settings['ACCOUNT'], settings['PASSWD'])
