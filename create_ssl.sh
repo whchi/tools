07724d47dc2a0f5ddeecac9f77f5d06eb66e1e12
whchi <whccchi@gmail.com>
2023-08-10 15:40:36 +0800

    Update create_ssl.sh

--- create_ssl.sh
+++ create_ssl.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-
 # Set the domain name variable (replace with your domain)
 DOMAIN_NAME="$1"
+# Check if the domain name is provided
+if [ -z "$DOMAIN_NAME" ]; then
+  echo "Please provide a domain name as an argument."
+  exit 1
+fi
+# Create a private key without passphrase
+openssl genpkey -algorithm RSA -out "${DOMAIN_NAME}.key"
 
-# Create a private key
-openssl genpkey -algorithm RSA -out "${DOMAIN_NAME}.key" -aes256
+# Set appropriate permissions
+chmod 400 "${DOMAIN_NAME}.key"
 
 # Create a certificate signing request (CSR)
 openssl req -new -key "${DOMAIN_NAME}.key" -out "${DOMAIN_NAME}.csr" -subj "/C=US/ST=State/L=Location/O=Organization/OU=Unit/CN=${DOMAIN_NAME}"
@@ -18,5 +24,5 @@ echo "Self-signed SSL certificate and key created for domain: ${DOMAIN_NAME}"
 # Optional: Print the certificate
 openssl x509 -in "${DOMAIN_NAME}.crt" -text -noout
 
-# Clean up the CSR (it's not needed anymore)
+# Clean up the CSR
 rm "${DOMAIN_NAME}.csr"
